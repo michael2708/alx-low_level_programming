079c4ee4e0f356aafb4333934db43be6e52ac7b3
michael2708 <akomolafem@gmail.com>
2021-08-26 08:26:01 -0700

    adds

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+int main()
+{
+    printf("Size of a char: %lu byte(s)\n", sizeof(char));
+    printf("Size of an int: %lu byte(s)\n", sizeof(int));
+    printf("Size of a float:%lu\n", sizeof(float));
+    return 0;

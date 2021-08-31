cbf394b9dfb0378505f2fe064eef5bb60c1320a9
michael2708 <akomolafem@gmail.com>
2021-08-31 13:26:58 -0700

    adds

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * _islower - checks for lowercase character
+ * _isalpha - checks for alphabetic characters
  *@c: character to check
  * Return: 1 if lowercase, 0 if uppercase
  */

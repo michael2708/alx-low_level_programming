c39c9b99f5023ac03974134110c2b38648534e7c
michael2708 <akomolafem@gmail.com>
2021-08-31 12:25:44 -0700

    adds

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - prints 10 times
+ *
+ * Return: Always 0 (Success)
+ */
+void print_alphabet_x10(void)
+{
+char alpha;
+int counter;
+for (counter = 0; counter < 10; counter++)
+{
+for (alpha = 'a'; alpha < 'z'; alpha++)
+{
+_putchar(alpha);
+}
+_putchar('\n');
+}
+}

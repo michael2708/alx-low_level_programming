63600a6b212df97c4ecd536c0e921adc5d21c8a7
michael2708 <akomolafem@gmail.com>
2021-09-02 07:55:54 -0700

    adds

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+* positive_or_negative - prints if positive or negative
+* Return: void
+*/
+
+void positive_or_negative(int i)
+{
+if (i < 0)
+{
+printf("%d is negative\n", i);
+}
+else if (i == 0)
+{
+printf("%d is zero\n", i);
+}
+else
+{
+printf("%d is positive\n", i);
+}
+}

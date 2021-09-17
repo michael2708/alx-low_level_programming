a9f01ab69ecf6986a17e6783ad9dd29338c20ca2
michael2708 <akomolafem@gmail.com>
2021-09-17 10:50:11 -0700

    adds

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+* main - Entry point
+* @argc: argument count
+* @argv: array vector
+* Return: Always (0) Success
+*/
+int main(int argc, char *argv[])
+{
+if (argc > 0)
+{
+printf("%s\n", argv[0]);
+}
+return (0);
+}

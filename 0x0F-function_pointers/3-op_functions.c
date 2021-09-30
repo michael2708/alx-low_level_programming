50df93467ebc0bbf8733eb1387f5970dc0478c6c
michael2708 <akomolafem@gmail.com>
2021-09-29 19:26:48 -0700

    adds

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,58 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "3-calc.h"
+
+/**
+ * op_add - function adds
+ * @a: first argument
+ * @b: second argument
+ * Return: a result
+ */
+int op_add(int a, int b)
+{
+return (a + b);
+}
+
+/**
+* op_sub - function substract
+* @a: first argument
+* @b: second argument
+* Return: a result
+*/
+int op_sub(int a, int b)
+{
+return (a - b);
+}
+
+/**
+* op_mul - function substract
+* @a: first argument
+* @b: second argument
+* Return: a result
+*/
+int op_mul(int a, int b)
+{
+return (a * b);
+}
+
+/**
+* op_div - function substract
+* @a: first argument
+* @b: second argument
+* Return: a result
+*/
+int op_div(int a, int b)
+{
+return (a / b);
+}
+
+/**
+* op_mod - function substract
+* @a: first argument
+* @b: second argument
+* Return: a result
+*/
+int op_mod(int a, int b)
+{
+return (a % b);
+}

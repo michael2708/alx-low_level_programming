2a03774558cb408d987e9d0849d26d50e72efc03
michael2708 <akomolafem@gmail.com>
2021-09-29 17:13:09 -0700

    adds

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -9,6 +9,5 @@
  */
 void print_name(char *name, void (*f)(char *))
 {
-f = &print_name_as_is;
-(*f)(name);
+f(name);
 }
--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,6 +1,5 @@
 #ifndef FUNCTION_POINTERS_H
 #define FUNCTION_POINTERS_H
 int _putchar(char c);
-void print_name_as_is(char *name);
 void print_name(char *name, void (*f)(char *));
 #endif
